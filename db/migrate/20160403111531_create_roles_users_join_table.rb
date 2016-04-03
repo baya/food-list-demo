3198d9b202c650dcad9691ad7ccc80a2192a3079
jgm <kayak.jiang@gmail.com>
2016-04-03 19:27:09 +0800

    add User & Role model

---
+++ db/migrate/20160403111531_create_roles_users_join_table.rb
@@ -0,0 +1,9 @@
+class CreateRolesUsersJoinTable < ActiveRecord::Migration
+  def change
+    create_table :roles_users do |t|
+      t.integer :user_id
+      t.integer :role_id
+      t.timestamps null: false
+    end
+  end
+end
